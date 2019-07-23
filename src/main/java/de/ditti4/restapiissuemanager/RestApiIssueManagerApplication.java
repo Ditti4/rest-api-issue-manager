aa46c68145ebc0326e94cff6e8952bf960fe369b
Rico Dittrich <ricod1996@gmail.com>
2019-07-23 20:36:36 +0200

    Add base application
    Thanks Spring Initializr (start.spring.io)!

---
+++ src/main/java/de/ditti4/restapiissuemanager/RestApiIssueManagerApplication.java
@@ -0,0 +1,13 @@
+package de.ditti4.restapiissuemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiIssueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiIssueManagerApplication.class, args);
+	}
+
+}
