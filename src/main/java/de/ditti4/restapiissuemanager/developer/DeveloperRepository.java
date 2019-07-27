bb0988b7d47f44cf2f2528afbe08c6742cbebf53
Rico Dittrich <ricod1996@gmail.com>
2019-07-27 18:23:15 +0200

    Remove RepositoryRestResource annotation
    Turns out, you don't even need that annotation if you don't want to do
    anything special in the repository. Spring Data REST takes care of
    everything. I'm amazed.

--- src/main/java/de/ditti4/restapiissuemanager/developer/DeveloperRepository.java
+++ src/main/java/de/ditti4/restapiissuemanager/developer/DeveloperRepository.java
@@ -1,8 +1,5 @@
 package de.ditti4.restapiissuemanager.developer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "developers")
-public interface DeveloperRepository extends JpaRepository<Developer, Long> {
-}
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {}
--- src/main/java/de/ditti4/restapiissuemanager/issue/IssueRepository.java
+++ src/main/java/de/ditti4/restapiissuemanager/issue/IssueRepository.java
@@ -1,8 +1,5 @@
 package de.ditti4.restapiissuemanager.issue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "issues", path = "issues")
-interface IssueRepository extends JpaRepository<Issue, Long> {
-}
+interface IssueRepository extends JpaRepository<Issue, Long> {}
