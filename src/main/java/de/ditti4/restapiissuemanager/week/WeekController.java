f6dc120f1adf1a11f94ad58fe0310ad38f8fc9c0
Rico Dittrich <ricod1996@gmail.com>
2019-08-03 14:52:57 +0200

    Assign stories to weeks
    This is just a work in progress and the whole idea of having stories
    with an estimate larger than 10 will be rethought.

--- src/main/java/de/ditti4/restapiissuemanager/week/WeekController.java
+++ src/main/java/de/ditti4/restapiissuemanager/week/WeekController.java
@@ -1,7 +1,11 @@
 package de.ditti4.restapiissuemanager.week;
 
+import de.ditti4.restapiissuemanager.developer.Developer;
 import de.ditti4.restapiissuemanager.developer.DeveloperRepository;
 import de.ditti4.restapiissuemanager.issue.IssueRepository;
+import de.ditti4.restapiissuemanager.issue.IssueType;
+import de.ditti4.restapiissuemanager.issue.story.Story;
+import de.ditti4.restapiissuemanager.issue.story.StoryStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.*;
 import org.springframework.hateoas.ResourceSupport;
@@ -13,7 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RepositoryRestController
 public class WeekController {
@@ -29,7 +37,32 @@ public class WeekController {
 
     @PostMapping("weeks")
     public ResponseEntity<ResourceSupport> post(PersistentEntityResourceAssembler assembler, @RequestBody Week newWeek) throws URISyntaxException {
+        List<Story> remainingStories = issueRepository.findAll().stream()
+                .filter(issue -> issue.getType() == IssueType.STORY)
+                .map(issue -> (Story) issue)
+                .filter(story -> story.getStatus() == StoryStatus.ESTIMATED)
+                .filter(story -> story.getRemainingEstimate() > 0)
+                .sorted(Comparator.comparing(Story::getId)) // let's try a FIFO approach
+                .collect(Collectors.toList());
+
+        for (Developer developer : developerRepository.findAll()) {
+            int workload = 0;
+            List<Story> assignedStories = new ArrayList<>();
+            for (Story story : remainingStories) {
+                if (workload < Week.MAXIMUM_WORKLOAD_PER_WEEK) {
+                    story.setDeveloper(developer);
+                    workload += story.getRemainingEstimate();
+                    newWeek.issues.add(story);
+                    assignedStories.add(story);
+                } else {
+                    break;
+                }
+            }
+            remainingStories.removeAll(assignedStories);
+        }
+
         newWeek = repository.save(newWeek);
+        issueRepository.saveAll(newWeek.getIssues());
 
         // The following code is pretty much copied from RepositoryEntityController's createAndReturn method
 
